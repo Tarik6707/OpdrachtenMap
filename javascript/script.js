49f6cb302f462a5ced24208e794685bc20b5d06e
Tarik6707 <9019582@student.zadkine.nl>
2022-10-11 13:55:55 +0200

    opdracht 5.1 javascript

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+alert("Tarik");
+
+document.getElementById("title").style.color = "red";
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 = getal2
+alert(getal1 + " x " + getal2 + " = " + som);
+
+var kleur = prompt("geef een engelse kleurnaam", "")
+document.getElementById("title").style.color = kleur;
+
+document.getElementById("p1").innerHTML = "hello "+ kleur +"!";
